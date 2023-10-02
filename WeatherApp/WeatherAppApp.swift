05f302e1bc1489b7f401573320418a15c72d11d0
Bhavuk <bhavuk@backbase.com>
2023-10-01 18:03:01 -0700

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Bhavuk Chawla on 01/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
